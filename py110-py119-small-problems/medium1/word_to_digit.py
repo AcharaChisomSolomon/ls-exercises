16ca2c613539f2c5b79a27c82e80c2652d43f63d
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-11 22:58:05 +0000

    word to digits done

---
+++ py110-py119-small-problems/medium1/word_to_digit.py
@@ -0,0 +1,22 @@
+def word_to_digit(message):
+    words = message.split(" ")
+    words_dict = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+
+    new_words = [words_dict[word] if word in words_dict else word for word in words]
+    return ' '.join(new_words)
+
+
+message = 'Please call me at five five five one two three four'
+print(word_to_digit(message) == "Please call me at 5 5 5 1 2 3 4")
+# Should print True
