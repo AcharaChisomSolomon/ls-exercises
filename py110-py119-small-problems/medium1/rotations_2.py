4e2885b7716adfcc93d4aff32728d38cd22f1513
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-11 21:41:39 +0000

    rotations part 2 done

---
+++ py110-py119-small-problems/medium1/rotations_2.py
@@ -0,0 +1,14 @@
+def rotate_rightmost_digits(number, position):
+    if position == 1: return number
+    lst = list(str(number))
+    new_lst = lst[:-position] + lst[-(position - 1):] + [lst[-position]]
+    return int(''.join(new_lst))
+
+
+print(rotate_rightmost_digits(735291, 2) == 735219)  # True
+print(rotate_rightmost_digits(735291, 3) == 735912)  # True
+print(rotate_rightmost_digits(735291, 1) == 735291)  # True
+print(rotate_rightmost_digits(735291, 4) == 732915)  # True
+print(rotate_rightmost_digits(735291, 5) == 752913)  # True
+print(rotate_rightmost_digits(735291, 6) == 352917)  # True
+print(rotate_rightmost_digits(1200, 3) == 1002)      # True
