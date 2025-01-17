d096d060aecb218132af54b74e83044236a5ac9c
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-17 16:51:43 +0000

    palindromic strings part 2 done

--- py110-py119-small-problems/easy1/palindromic_string.py
+++ py110-py119-small-problems/easy1/palindromic_string.py
@@ -9,15 +9,32 @@ def is_palindrome(string):
 
     return True
 
+def is_real_palindrome(string):
+    new_string = ''.join([char.lower() for char in list(string) if char.isalnum()])
+    return is_palindrome(new_string)
 
-# All of these examples should print True
 
-print(is_palindrome('madam') == True)
-print(is_palindrome('356653') == True)
-print(is_palindrome('356635') == False)
+# # All of these examples should print True
 
-# case matters
-print(is_palindrome('Madam') == False)
+# print(is_palindrome('madam') == True)
+# print(is_palindrome('356653') == True)
+# print(is_palindrome('356635') == False)
 
-# all characters matter
-print(is_palindrome("madam i'm adam") == False)
+# # case matters
+# print(is_palindrome('Madam') == False)
+
+# # all characters matter
+# print(is_palindrome("madam i'm adam") == False)
+
+
+print(is_real_palindrome('madam') == True)           # True
+print(is_real_palindrome('356653') == True)          # True
+print(is_real_palindrome('356635') == False)         # True
+print(is_real_palindrome('356a653') == True)         # True
+print(is_real_palindrome('123ab321') == False)       # True
+
+# case doesn't matter
+print(is_real_palindrome('Madam') == True)           # True
+
+# only alphanumerics matter
+print(is_real_palindrome("Madam, I'm Adam") == True) # True
