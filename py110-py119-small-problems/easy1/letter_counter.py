a5d1e462643fa4e02d070dd86bd53d0201fb201d
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-17 22:50:32 +0000

    letter counter part 2 done

--- py110-py119-small-problems/easy1/letter_counter.py
+++ py110-py119-small-problems/easy1/letter_counter.py
@@ -1,3 +1,6 @@
+def remove_non_letters(word):
+    return ''.join([char for char in list(word) if char.isalpha()])
+
 def word_sizes(string):
     output = {}
 
@@ -5,6 +8,8 @@ def word_sizes(string):
         return output
     
     words = string.split(' ')
+    words = [remove_non_letters(word) for word in words]
+
     for word in words:
         if len(word) in output:
             output[len(word)] = output[len(word)] + 1
@@ -16,15 +21,15 @@ def word_sizes(string):
 # All of these examples should print True
 
 string = 'Four score and seven.'
-print(word_sizes(string) == {4: 1, 5: 1, 3: 1, 6: 1})
+print(word_sizes(string) == {4: 1, 5: 2, 3: 1})
 
 string = 'Hey diddle diddle, the cat and the fiddle!'
-print(word_sizes(string) == {3: 5, 6: 1, 7: 2})
+print(word_sizes(string) == {3: 5, 6: 3})
 
-string = 'Humpty Dumpty sat on a wall'
-print(word_sizes(string) == {6: 2, 3: 1, 2: 1, 1: 1, 4: 1})
+string = 'Humpty Dumpty sat on a w@ll'
+print(word_sizes(string) == {6: 2, 3: 2, 2: 1, 1: 1})
 
 string = "What's up doc?"
-print(word_sizes(string) == {6: 1, 2: 1, 4: 1})
+print(word_sizes(string) == {5: 1, 2: 1, 3: 1})
 
 print(word_sizes('') == {})
