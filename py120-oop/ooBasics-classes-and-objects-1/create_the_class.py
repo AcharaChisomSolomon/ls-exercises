a178dfcee7c23d364b717b5b4b6ec1f7d47caed5
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-20 19:03:54 +0000

    getter done

--- py120-oop/ooBasics-classes-and-objects-1/create_the_class.py
+++ py120-oop/ooBasics-classes-and-objects-1/create_the_class.py
@@ -2,8 +2,12 @@ class Cat:
     def __init__(self, name):
         self._name = name
 
+    @property
+    def name(self):
+        return self._name
+
     def greet(self):
-        print(f"Hello! My name is {self._name}!")
+        print(f"Hello! My name is {self.name}!")
 
 kitty = Cat('Sophie')
 kitty.greet()
