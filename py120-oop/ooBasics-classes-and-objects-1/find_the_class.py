f0d16b36ddbd6f387bca8fe6386208c7c880294b
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-20 15:06:14 +0000

    find the class done

---
+++ py120-oop/ooBasics-classes-and-objects-1/find_the_class.py
@@ -0,0 +1,4 @@
+# Comments show expected output
+print("Hello".__class__)                # <class 'str'>
+print((5).__class__)                      # <class 'int'>
+print([1, 2, 3].__class__)              # <class 'list'>
